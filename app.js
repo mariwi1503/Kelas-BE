c2a983d2b81f8e490df573bf6d9f4c86daa345ae
Widodo <widodroid@gmail.com>
2024-07-01 15:14:44 +0700

    feat: Add CRUD and Nodemon

--- app.js
+++ app.js
@@ -26,10 +26,17 @@ app.get("/users", (req, res) => {
 app.get("/users/:name", (req, res) => {
   const name = req.params.name;
   const user = users.find((x) => x.name == name);
-  res.json({
-    status: "OK",
-    data: user,
-  });
+  if (!user) {
+    res.status(404).json({
+      status: "Not Found",
+      message: "User not found",
+    });
+  } else {
+    res.json({
+      status: "OK",
+      data: user,
+    });
+  }
 });
 
 // TODO =
@@ -51,26 +58,48 @@ app.post("/add-user/:name/:age", (req, res) => {
 app.put("/update-user/:name/:age", (req, res) => {
   const name = req.params.name;
   const age = req.params.age;
+  let userUpdated = false;
+
   users = users.map((x) => {
-    if (x.name == name) {
-      x.age = age;
+    if (x.name === name) {
+      x.age = parseInt(age);
+      userUpdated = true;
     }
     return x;
   });
-  res.json({
-    status: "OK",
-    data: users,
-  });
+
+  if (userUpdated) {
+    res.json({
+      status: "OK",
+      message: `User '${name}' has been successfully updated.`,
+      data: users,
+    });
+  } else {
+    res.status(404).json({
+      status: "Not Found",
+      message: `User '${name}' not found.`,
+    });
+  }
 });
 
 // hapus user
 app.delete("/delete-user/:name", (req, res) => {
-  const name = req.params.name;
-  users = users.filter((x) => x.name != name);
-  res.json({
-    status: "OK",
-    data: users,
-  });
+  const deletedUserName = req.params.name;
+  const updatedUsers = users.filter(({ name }) => name !== deletedUserName);
+
+  if (updatedUsers.length < users.length) {
+    res.json({
+      status: "OK",
+      message: `User '${deletedUserName}' has been successfully deleted.`,
+      data: updatedUsers,
+    });
+  } else {
+    // User not found
+    res.status(404).json({
+      status: "Not Found",
+      message: `User '${deletedUserName}' not found.`,
+    });
+  }
 });
 
 app.listen(3000, () => console.log("Server is listening..."));
