9b9b9d1a0ce25eb47526b47969125ea18e26149f
Agus27111 <agussetiawanbelajar@gmail.com>
2024-07-17 20:41:59 +0700

    done all CRUD

--- pertemuan_4/controllers/unit-kegiatan-mahasiswa.controller.js
+++ pertemuan_4/controllers/unit-kegiatan-mahasiswa.controller.js
@@ -1,5 +1,5 @@
 const ukmModel = require("../model/unit-kegiatan-mahasiswa.model");
-const { getAll } = require("./mahasiswa.controller");
+const { getAll, getOneById } = require("./mahasiswa.controller");
 
 module.exports = {
   getAll: async (req, res) => {
@@ -11,6 +11,93 @@ module.exports = {
       });
     } catch (error) {
       res.status(404).json({
+        status: "failed",
+        message: error.message,
+      });
+    }
+  },
+  getOneById: async (req, res) => {
+    try {
+      const id = +req.params.id;
+      const ukm = await ukmModel.findOne({ where: { id } });
+      res.status(200).json({
+        status: "success",
+        data: ukm ?? null,
+      });
+    } catch (error) {
+      res.status(400).json({
+        status: "failed",
+        message: error.message,
+      });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const nama = req.body.nama;
+      const tentang = req.body.tentang;
+      const jumlah = req.body.jumlah;
+
+      if (!nama || nama === "") throw new Error("Nama tidak boleh kosong");
+      if (!tentang || tentang === "")
+        throw new Error("Tentang tidak boleh kosong");
+      if (!jumlah || jumlah === "")
+        throw new Error("Jumlah tidak boleh kosong");
+
+      await ukmModel.create({ nama, tentang, jumlah });
+      res.status(200).json({
+        status: "success",
+      });
+    } catch (error) {
+      res.status(404).json({
+        status: "failed",
+        message: error.message,
+      });
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const id = +req.params.id;
+
+      const nama = req.body.nama;
+      const tentang = req.body.tentang;
+      const jumlah = req.body.jumlah;
+
+      // cek data ukmUpdate
+      const ukmUpdate = await ukmModel.findOne({ where: { id } });
+      if (!ukmUpdate) throw new Error("Data tidak ditemukan");
+
+      // update data ukmUpdate
+      await ukmModel.update({ nama, tentang, jumlah }, { where: { id } });
+
+      // kirim response berhasil ke user
+      res.status(200).json({
+        status: "success",
+      });
+    } catch (error) {
+      res.status(400).json({
+        status: "failed",
+        message: error.message,
+      });
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const id = +req.params.id;
+
+      // cek data mataKuliah
+      const ukmDelete = await ukmModel.findOne({ where: { id } });
+      if (!ukmDelete) throw new Error("Data tidak ditemukan");
+
+      // hapus data
+      await ukmModel.destroy({ where: { id } });
+
+      // kirim response berhasil ke user
+      res.status(200).json({
+        status: "success",
+      });
+    } catch (error) {
+      res.status(400).json({
         status: "failed",
         message: error.message,
       });
--- pertemuan_4/router/unit-kegiatan-mahasiswa.route.js
+++ pertemuan_4/router/unit-kegiatan-mahasiswa.route.js
@@ -1,6 +1,10 @@
 const router = require("express").Router();
 const ukmController = require("../controllers/unit-kegiatan-mahasiswa.controller");
 
+router.post("/ukm", ukmController.create);
 router.get("/ukm", ukmController.getAll);
+router.get("/ukm/:id", ukmController.getOneById);
+router.put("/ukm/:id", ukmController.update);
+router.delete("/ukm/:id", ukmController.delete);
 
 module.exports = router;
